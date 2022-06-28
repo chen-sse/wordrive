45759e6e91c755d93d90bab272483a585b3ff0be
Andrew Chen <andyschen2013@gmail.com>
2022-06-28 18:50:13 -0400

    Implement click handler that saves selected text to Chrome sync storage

--- background.js
+++ background.js
@@ -1,7 +1,26 @@
+// create context menu
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         id: "contextMenu",
         title: "Add to Wordrive",
-        contexts: ["selection"]
+        contexts: ["selection"],
     });
-})
+});
+
+// click handler--add text to Wordrive
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    chrome.storage.sync.get("words", (data) => {
+        // if word bank array has not been initialized
+        if (typeof data.words === "undefined") {
+            chrome.storage.sync.set({"words": []});
+            chrome.storage.sync.get("words", (updatedData) => {
+                updatedData.words.push(info.selectionText);
+                chrome.storage.sync.set({"words": updatedData.words});
+            });
+        // if word bank array already exists
+        } else {
+            data.words.push(info.selectionText);
+            chrome.storage.sync.set({"words": data.words});
+        }
+    });
+});
