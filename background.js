b2b09c4caead4d1287034120d7d7ae93537ff6ab
Andrew Chen <andyschen2013@gmail.com>
2022-07-04 19:07:07 -0400

    Refactor background.js

--- background.js
+++ background.js
@@ -9,44 +9,21 @@ chrome.runtime.onInstalled.addListener(() => {
 
 // click handler: add text to Wordrive
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    chrome.storage.sync.get("words", (data) => {
-        // if word bank array has not been initialized
-        if (typeof data.words === "undefined") {
-            // create empty array
-            chrome.storage.sync.set({"words": []});
-            chrome.storage.sync.get("words", (updatedData) => {
-                chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-                    chrome.storage.sync.get({"lowercase": true}, (settings) => {
-                        // add new word object to array
-                        updatedData.words.push({
-                            text: (settings.lowercase === true) 
-                            ? info.selectionText.toLowerCase() 
-                            : info.selectionText,
+    chrome.storage.sync.get({"words": []}, (data) => {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
+            chrome.storage.sync.get({"lowercase": true}, (settings) => {
+            // add new word object to array
+            data.words.push({
+                text: (settings.lowercase === true)
+                ? info.selectionText.toLowerCase()
+                : info.selectionText,
 
-                            url: tabs[0].url
-                        });
-                        // set key to updated array
-                        chrome.storage.sync.set({"words": updatedData.words});
-                    });
-                });
+                url: tabs[0].url
             });
-        // if word bank array already exists
-        } else {
-            chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-                chrome.storage.sync.get({"lowercase": true}, (settings) => {
-                // add new word object to array
-                data.words.push({
-                    text: (settings.lowercase === true)
-                    ? info.selectionText.toLowerCase()
-                    : info.selectionText,
-                    
-                    url: tabs[0].url
-                });
-                // set key to updated array
-                chrome.storage.sync.set({"words": data.words});
-                });
+            // set key to updated array
+            chrome.storage.sync.set({"words": data.words});
             });
-        }
+        });
     });
 });
 
@@ -58,11 +35,10 @@ chrome.runtime.onMessage.addListener((request) => {
             // default values
             activeTab: false
         }, (settings) => {
-            if (settings.activeTab === true) {
-                chrome.tabs.create({url: request.url, active: true});
-            } else {
-                chrome.tabs.create({url: request.url, active: false});
-            }
+            chrome.tabs.create({
+                url: request.url,
+                active: (settings.activeTab) ? true : false
+            });
         });
     }
 });
