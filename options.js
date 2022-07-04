dc2726cd82da83d754df508c777363cc29b4524b
Andrew Chen <andyschen2013@gmail.com>
2022-07-04 17:36:48 -0400

    Add options menu to allow for user customization
    Add option for clicked hyperlinks to automatically become the active tab.

---
+++ options.js
@@ -0,0 +1,26 @@
+const SAVE_DISPLAY_TIME = 2000;
+
+// save options to Chrome sync
+function saveOptions() {
+    let activeTab = document.getElementById("activeTab").checked;
+    chrome.storage.sync.set({"activeTab": activeTab}, () => {  // check if string representation of property is necessary
+        let status = document.getElementById("status");
+        status.innerHTML = "Options saved.";
+        setTimeout(() => {
+            status.innerHTML = "";
+        }, SAVE_DISPLAY_TIME);
+    });
+}
+
+// restore saved user preferences
+function restoreOptions() {
+    chrome.storage.sync.get({
+        // default values
+        "activeTab": false
+    }, (settings) => {
+        document.getElementById("activeTab").checked = settings.activeTab;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.getElementById("save").addEventListener("click", saveOptions);
