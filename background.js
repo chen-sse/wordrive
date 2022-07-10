23bf1adb356eb3b98612dd6a114351ccd275e87d
Andrew Chen <andyschen2013@gmail.com>
2022-07-10 00:35:09 -0400

    Remove extra space from background.js

--- background.js
+++ background.js
@@ -20,7 +20,7 @@ chrome.runtime.onInstalled.addListener(() => {
 // click handler: add text to Wordrive
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     // retrieve word bank and user capitalization preference (initialize default values for both)
-    chrome.storage.sync.get(["wordBank", "lowercaseChecked"],  async (data) => {
+    chrome.storage.sync.get(["wordBank", "lowercaseChecked"], async (data) => {
         let [currentTab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
         // add new word object to array
         let selectedWord = info.selectionText.trim();
