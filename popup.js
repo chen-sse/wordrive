14c70cfbe58cc727ca08eb80c889b804beb2d20f
Andrew Chen <andyschen2013@gmail.com>
2022-07-11 15:06:45 -0400

    Remove unnecessary array init check and edit comments
    With the implementation of a default value setter attached to the 'onInstalled' event, checking for the existence of the 'wordBank' array is no longer necessary.

--- popup.js
+++ popup.js
@@ -26,53 +26,50 @@ function toggleButton(word, button, data, i) {
 }
 
 chrome.storage.sync.get("wordBank", (data) => {
-    // if word bank array has been initialized
-    if (typeof data.wordBank !== "undefined") {
-        for (let i = 0; i < data.wordBank.length; i++) {
-            // init a div-span-button set for given word (box div as parent element)
-            let box = document.createElement("div");
-            let word = document.createElement("span");
-            let button = document.createElement("button");
-            let wordUrl = data.wordBank[i].url;
+    for (let i = 0; i < data.wordBank.length; i++) {
+        // init a div-span-button set for given word (box div as parent element)
+        let box = document.createElement("div");
+        let word = document.createElement("span");
+        let button = document.createElement("button");
+        let wordUrl = data.wordBank[i].url;
 
-            // add classes to box and button
-            button.classList.add("edit");
-            box.classList.add("entry");
+        // add classes to 'box' and 'button'
+        button.classList.add("edit");
+        box.classList.add("entry");
 
-            // init attributes href and contenteditable to span element
-            word.setAttribute("href", wordUrl);
-            word.setAttribute("contenteditable", false);
-            word.innerText = data.wordBank[i].text;
-            button.innerHTML = "Edit";
+        // init attributes 'href' and 'contenteditable' to span element
+        word.setAttribute("href", wordUrl);
+        word.setAttribute("contenteditable", false);
+        word.innerText = data.wordBank[i].text;
+        button.innerHTML = "Edit";
 
-            // make button and word children of box
-            box.appendChild(button);
-            box.appendChild(word);
-            wordsDiv.appendChild(box);
+        // make 'button' and 'word' children of 'box'
+        box.appendChild(button);
+        box.appendChild(word);
+        wordsDiv.appendChild(box);
 
-            // click handler: tell background script to open hyperlink
-            word.addEventListener("click", () => {
-                // if not in edit mode
-                if (word.isContentEditable === false) {
-                    chrome.runtime.sendMessage({
-                        msg: "new tab",
-                        url: wordUrl
-                    });
-                }
-            });
+        // click handler: tell background script to open hyperlink
+        word.addEventListener("click", () => {
+            // if not in edit mode
+            if (word.isContentEditable === false) {
+                chrome.runtime.sendMessage({
+                    msg: "new tab",
+                    url: wordUrl
+                });
+            }
+        });
 
-            // save changes and exit edit mode with 'Enter'
-            word.addEventListener("keydown", (event) => {
-                if (event.code === "Enter") {
-                    toggleButton(word, button, data, i);
-                }
-            });
-
-            // toggle edit/save button
-            button.addEventListener("click", () => {
+        // save changes and exit edit mode with 'Enter'
+        word.addEventListener("keydown", (event) => {
+            if (event.code === "Enter") {
                 toggleButton(word, button, data, i);
-            });
-        }
+            }
+        });
+
+        // toggle edit/save button on click
+        button.addEventListener("click", () => {
+            toggleButton(word, button, data, i);
+        });
     }
 });
 
