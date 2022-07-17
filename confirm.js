ef5008556f5a50182d2a7a03a2f818d527a6576b
Andrew Chen <andyschen2013@gmail.com>
2022-07-16 23:54:07 -0400

    Rewrite confirm.js Promise chain using async/await for readability

--- confirm.js
+++ confirm.js
@@ -1,15 +1,14 @@
-// fire SweetAlert popup
-Swal.fire({
-    title: "Are you sure you want to clear your Wordrive?",
-    text: "You won't be able to undo this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!"
-}).then((result) => {
-    // if user confirms deletion
-    if (result.isConfirmed) {
+async function fireAlert() {
+    let response = await Swal.fire({
+        title: "Are you sure you want to clear your Wordrive?",
+        text: "You won't be able to undo this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    });
+    if (response.isConfirmed) {
         Swal.fire(
             "Deleted!",
             "Your Wordrive has been cleared.",
@@ -18,4 +17,7 @@ Swal.fire({
         // tell background script to clear Wordrive
         chrome.runtime.sendMessage({msg: "clear"});
     }
-});
+}
+
+// fire SweetAlert popup
+fireAlert();
