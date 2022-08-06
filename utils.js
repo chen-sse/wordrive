45cf2b6fdb38fc7ab12e99fbfd57e0c69a57e897
Andrew Chen <andyschen2013@gmail.com>
2022-08-06 15:39:07 -0400

    Add utils.js module to store utility functions
    Move getDate(), getTime(), and getDictionaryURL() into the utils.js module so that they can be imported and invoked outside of background.js. background.js is registered as a module in manifest.json to enable the 'import' keyword.

---
+++ utils.js
@@ -0,0 +1,36 @@
+// fetch date from local machine
+function getDate() {
+    const dateOptions = {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+    return new Date().toLocaleDateString("en-US", dateOptions);
+}
+
+// fetch time from local machine
+function getTime() {
+    const timeOptions = {
+        hour: "numeric",
+        minute: "numeric",
+        timeZoneName: "short"
+    };
+    let time = new Date().toLocaleTimeString("en-US", timeOptions).replace("AM", "am").replace("PM", "pm");
+    const timeTokens = time.split(" ");
+    return `${timeTokens[0]}${timeTokens[1]} ${timeTokens[2]}`;
+}
+
+// generate the Merriam-Webster dictionary URL of a given term
+function getDictionaryURL(term) {
+    // tokenize term (applies to multi-word queries)
+    const searchTokens = term.match(/\S+/g);
+    // construct dictionary URL
+    let dictionaryUrl = "https://www.merriam-webster.com/dictionary/";
+    for (let j = 0; j < searchTokens.length; j++) {
+        dictionaryUrl += searchTokens[j];
+        dictionaryUrl += "%20";
+    }
+    return dictionaryUrl;
+}
+
+export { getDate, getTime, getDictionaryURL };
